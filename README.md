4b42f4ce90a551045853f50f1cac4b6bbaf563ab
niguanting@gmail.com <niguangting@gmail.com>
2018-07-23 16:04:53 -0700

    Version 6 Authorization

--- README.md
+++ README.md
@@ -31,6 +31,23 @@ Version 3
 
     Add seeds file
 
+    RESTFUL ROUTES
+
+    name      url      verb    desc.
+    ===============================================
+    INDEX   /dogs      GET   Display a list of all dogs
+    NEW     /dogs/new  GET   Displays form to make a new dog
+    CREATE  /dogs      POST  Add new dog to DB
+    SHOW    /dogs/:id  GET   Shows info about one dog
+
+    INDEX   /campgrounds
+    NEW     /campgrounds/new
+    CREATE  /campgrounds
+    SHOW    /campgrounds/:id
+
+    NEW     campgrounds/:id/comments/new    GET
+    CREATE  campgrounds/:id/comments      POST
+
 Version 4
 
     Add Comment routes
@@ -39,7 +56,14 @@ Version 5
 
    Add stylesheet
 
-   
+Version 6
+    Authorization
+
+    Add login/logout/Register routes
+
+
+
+
 
 
 
--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
+    User        = require("./models/user"),
     seedDB      = require("./seeds")
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,8 +18,26 @@ var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
 mongoose.connect(url, {
     useMongoClient: true,
 });
+app.use(express.static(__dirname + "/public"));
 
 //seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -104,6 +125,53 @@ app.post("/campgrounds/:id/comments", function(req, res){
         }
     });
 });
+
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+// handling login logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+// logic route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 const PORT = process.env.PORT || 5996;
